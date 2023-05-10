0ca7827e1cbc97ddbf787f803109f4c55c20a583
XCODE-S-AVEX41 <aleksander.hoff06@gmail.com>
2023-05-10 11:04:39 +0200

    Made a switch to create another one, and informed the user that is is saved

--- rememberer/CreationView.swift
+++ rememberer/CreationView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct CreationView: View {
     @State var tList: String = ""
-    @State var saved = false
+    @State var notsaved = false
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Page.name, ascending: true)],
@@ -19,12 +19,16 @@ struct CreationView: View {
     
     var body: some View {
         Form {
-            Section(header: Text("Name for new Task List")) {
-                TextField("Task Name", text: $tList)
-            }/*
-            Section() {
-                Button("Create new Task list", action: Submiter)
-            }*/
+            if !notsaved {
+                Text("Saved successful, you can go back now!")
+                Toggle(isOn: $notsaved) {
+                    Label("Or, create another one", systemImage: "rectangle.and.pencil.and.ellipsis")
+                }
+            } else {
+                Section(header: Text("Name for new Task List")) {
+                    TextField("Task Name", text: $tList)
+                }
+            }
         }
         .navigationBarTitle("Settings")
         .onSubmit {
@@ -39,14 +43,14 @@ struct CreationView: View {
 
             do {
                 try viewContext.save()
-                saved = true
+                notsaved = true
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
-            if saved {
+            if notsaved {
                 ContentView()
             }
         }
